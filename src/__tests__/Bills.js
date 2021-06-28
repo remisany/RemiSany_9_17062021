1b87a6ad4f9b65a65ae04d043c809bfbb2e0934f
remisany <remi.d.sany@gmail.com>
2021-06-28 17:04:59 +0200

    Ajout tests unitaires et d'intégration GET (Bills) Taux de couverture : 62.5%

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,6 +1,13 @@
 import { screen } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
+import userEvent from '@testing-library/user-event'
+
+import Bill from "../containers/Bills.js"
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import { ROUTES } from "../constants/routes"
+
+import firebase from "../__mocks__/firebase";
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
@@ -16,6 +23,75 @@ describe("Given I am connected as an employee", () => {
       const antiChrono = (a, b) => ((a < b) ? 1 : -1)
       const datesSorted = [...dates].sort(antiChrono)
       expect(dates).toEqual(datesSorted)
+    })
+  })
+  describe("When I am on Bills Page and I click on the new bill button", () => {
+    test("Then a new bill page should be open", () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const newBill = new Bill({ document, onNavigate, firestore: null, localStorage: window.localStorage })
+      const handleClickNewBill = jest.fn(newBill.handleClickNewBill)
+      const buttonNewBill = screen.getByTestId("btn-new-bill")
+      buttonNewBill.addEventListener("click", handleClickNewBill)
+      userEvent.click(buttonNewBill)
+      expect(handleClickNewBill).toHaveBeenCalled()
+      expect(screen.getByText('Envoyer une note de frais')).toBeTruthy()
+    })
+  })
+  describe("When I am on Bills Page and I click on the iconEye of one of the bills", () => {
+    test("Then a modal with an image should be open", () => {
+      const html = BillsUI({ data: bills })
+      document.body.innerHTML = html
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const newBill = new Bill({ document, onNavigate, firestore: null, localStorage: window.localStorage })
+      const iconEye = screen.getAllByTestId("icon-eye")[0]
+      $.fn.modal = jest.fn();
+      const handleClickIconEye = jest.fn(newBill.handleClickIconEye(iconEye))
+      iconEye.addEventListener("click", handleClickIconEye)
+      userEvent.click(iconEye)     
+      expect(handleClickIconEye).toHaveBeenCalled()
+      expect(screen.getByText('Justificatif')).toBeTruthy()
+    })
+  })
+})
+
+//test d'intégration GET
+describe("Given I am a user connected as Employee", () => {
+  describe("When I navigate to Bills UI", () => {
+    test("fetches bills from mock API GET", async () => {
+       const getSpy = jest.spyOn(firebase, "get")
+       const bills = await firebase.get()
+       expect(getSpy).toHaveBeenCalledTimes(1)
+       expect(bills.data.length).toBe(4)
+    })
+    test("fetches bills from an API and fails with 404 message error", async () => {
+      firebase.get.mockImplementationOnce(() =>
+        Promise.reject(new Error("Erreur 404"))
+      )
+      const html = BillsUI({ error: "Erreur 404" })
+      document.body.innerHTML = html
+      const message = await screen.getByText(/Erreur 404/)
+      expect(message).toBeTruthy()
+    })
+    test("fetches messages from an API and fails with 500 message error", async () => {
+      firebase.get.mockImplementationOnce(() =>
+        Promise.reject(new Error("Erreur 500"))
+      )
+      const html = BillsUI({ error: "Erreur 500" })
+      document.body.innerHTML = html
+      const message = await screen.getByText(/Erreur 500/)
+      expect(message).toBeTruthy()
     })
   })
 })
