a7295aff8739babf8e782fbddd4e1b4e3bff3160
remisany <remi.d.sany@gmail.com>
2021-06-30 18:14:34 +0200

    Corrections mineures

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,20 +2,13 @@ import { screen } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import userEvent from '@testing-library/user-event'
-
 import Bill from "../containers/Bills.js"
-import { localStorageMock } from "../__mocks__/localStorage.js";
+import { localStorageMock } from "../__mocks__/localStorage.js"
 import { ROUTES } from "../constants/routes"
-
-import firebase from "../__mocks__/firebase";
+import firebase from "../__mocks__/firebase"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
-    test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({ data: []})
-      document.body.innerHTML = html
-      //to-do write expect expression
-    })
     test("Then bills should be ordered from earliest to latest", () => {
       const html = BillsUI({ data: bills })
       document.body.innerHTML = html
